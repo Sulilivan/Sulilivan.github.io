9224b2cb1d599d1608af9c292236f4a9ea2411a0
Sullivan <2232618782s@gmail.com>
2025-10-31 00:13:33 +0800

    个人主页更新

--- Sulilivan.js
+++ Sulilivan.js
@@ -1,2 +1,8 @@
 // 目前没有功能性 JS，如需交互可在此添加
 console.log("欢迎来到沙粒文的主页！");
+
+document.querySelectorAll('#github a').forEach(function(link) {
+    link.addEventListener('click', function() {
+        console.log("正在跳转到：" + link.href);
+    });
+});
